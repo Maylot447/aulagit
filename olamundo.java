74f1253f69bcf5ce7f7d1d2cc7d724c90254727d
Maylot447 <maylotnhamajabotomo@gmail.com>
2025-11-20 20:27:13 +0200

    conversa aleatoria

--- olamundo.java
+++ olamundo.java
@@ -2,5 +2,7 @@ import java.io.*;
 public class olamundo{
     public static void main (String []args)throws IOException{
     System.out.println("ola mundo");
+    System.out.println("EU: estou bem graças a Deus e vc?");
+    System.out.println("ELA: tambem estou bem na mesma  graça");
 }
 }
